02d6e26d96d43e1d2dcbc25bb34c93a7d4c7f45b
Vyacheslav <volkovs976@rambler.ru>
2025-11-25 09:26:27 +0300

    Commit_1

--- pages/ui_page.py
+++ pages/ui_page.py
@@ -1,5 +1,6 @@
 import allure
 import re
+from time import sleep
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -12,61 +13,66 @@ class MainPage:
         self.driver.maximize_window()
         self.close_communication_if_exists()
 
-    def _wait_for_elements(self, by, value, multiple=False, timeout=10):
-        if multiple:
-            return WebDriverWait(
-                self.driver, timeout).until(
-                EC.visibility_of_all_elements_located(
-                    (by, value)))
-        else:
-            return WebDriverWait(
-                self.driver, timeout).until(
-                EC.visibility_of_element_located(
-                    (by, value)))
-
+    @allure.step("Проверка наличия и закрытие всплывающего окна, если оно появляется")
     def close_communication_if_exists(self, timeout=10):
-        element = WebDriverWait(
-            self.driver,
-            timeout).until(
-            EC.any_of(
-                EC.presence_of_element_located(
-                    (By.CSS_SELECTOR,
-                     '[aria-label="Закрыть коммуникацию"]')),
-                EC.presence_of_element_located(
-                    (By.CSS_SELECTOR,
-                     '[name="kp_query"]'))))
+        """
+        Ожидает появления либо кнопки закрытия коммуникации,
+        либо поля поиска (признак, что окна нет)
+        """
+        # Ждем появления любого из двух элементов
+        element = WebDriverWait(self.driver,timeout).until(
+            EC.any_of(EC.presence_of_element_located((By.CSS_SELECTOR,
+                                                      '[aria-label="Закрыть коммуникацию"]')),
+                # Поле поиска (индикатор нормальной страницы)
+                EC.presence_of_element_located((By.CSS_SELECTOR,'[name="kp_query"]'))
+            )
+        )
+        # Проверяем, что именно нашли
         if element.get_attribute("aria-label") == "Закрыть коммуникацию":
             element.click()
 
-    def _get_element_texts(self, css_selector):
-        elements = self._wait_for_elements(
-            By.CSS_SELECTOR, css_selector, multiple=True)
-        return [element.text for element in elements]
+    @allure.step("Ожидание появления элементов на странице со значением value")
+    def _wait_for_elements(self, by, value, multiple=False, timeout=10):
 
-    @allure.step("Получение заголовка страницы")
-    def check_page_title(self, expected_title):
-        WebDriverWait(self.driver, 10).until(lambda driver: driver.title != "")
-        return self.driver.title == expected_title
+            if multiple:
+                return WebDriverWait(self.driver, timeout).until(EC.visibility_of_all_elements_located((by, value)))
+            else:
+                return WebDriverWait(self.driver, timeout).until(EC.visibility_of_element_located((by, value)))
+
+    @allure.step("Получение текста со всех элементов страницы соотв. css_selector")
+    def _get_element_texts(self, css_selector):
+        elements = self._wait_for_elements(By.CSS_SELECTOR, css_selector, multiple=True)
+        return [element.text for element in elements]
 
     @allure.step("Поиск контента по фразе: {phrase}")
     def search_items_by_phrase(self, phrase):
-        self._wait_for_elements(By.CSS_SELECTOR,
-                                "input[name=kp_query]").send_keys(phrase)
-        self.driver.find_element(
-            By.CSS_SELECTOR,
-            "button[type=submit]").click()
+        with allure.step("Поиск поля ввода названия фильма и ввод названия"):
+            self._wait_for_elements(By.CSS_SELECTOR,
+                                "input[name=kp_query]" ).send_keys(phrase)
+        with allure.step("Нахождение и выбор кнопки Все результаты"):
+            self._wait_for_elements(By.CSS_SELECTOR, "#all").click()
 
-    @allure.step("Получаем названия фильмов")
-    def find_book_titles(self):
-        return self._get_element_texts("[data-type='film']")
+        with allure.step("Нахождение и нажатие кнопки Поиск"):
+            self.driver.find_element(By.CSS_SELECTOR,"button[aria-label='Найти']").click()
 
     @allure.step("Получаем количество результатов поиска")
     def get_search_results_count(self):
-        results_text = self._wait_for_elements(
-            By.CSS_SELECTOR, ".search_results_topText").text
+        with allure.step("Ожидает появления элемента с классом .search_results_topText и извлекает из него текст"):
+            results_text = self._wait_for_elements(By.CSS_SELECTOR, ".search_results_topText").text
+        with allure.step("Поиск числа в results_text с помощью регулярного выражения"):
+            match = re.search(r'результаты:\s*(\d+)', results_text)
+        with allure.step("Обработка результата поиска"):
+            if match:
+                return int(match.group(1))
+            else:
+                return 0
 
-        match = re.search(r'результаты:\s*(\d+)', results_text)
-        if match:
-            return int(match.group(1))
-        else:
-            return 0
+    @allure.step("Получаем названия фильмов")
+    def find_films_titles(self):
+
+        return self._get_element_texts("[data-type='film']")
+
+    @allure.step("Проверка заголовка страницы")
+    def check_page_title(self, expected_title):
+        WebDriverWait(self.driver, timeout=10).until(lambda driver: driver.title != "")
+        return self.driver.title == expected_title
--- tests/test_ui.py
+++ tests/test_ui.py
@@ -1,12 +1,14 @@
 import pytest
 import allure
-from pages.main_page import MainPage
+from pages.ui_page import MainPage
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from config import MAIN_URL, TITLE
 
 
 @pytest.fixture
 def driver():
+    """Фикстура для создания драйвера"""
     driver = webdriver.Chrome()
     yield driver
     driver.quit()
@@ -14,6 +16,7 @@ def driver():
 
 @pytest.fixture
 def main_page(driver):
+    """Фикстура для создания главной страницы"""
     driver.get(MAIN_URL)
     driver.get(MAIN_URL)
     return MainPage(driver, MAIN_URL)
@@ -24,6 +27,7 @@ def main_page(driver):
 @allure.title("Проверка заголовка главной страницы")
 @pytest.mark.smoke
 def test_check_main_page_title(main_page):
+    """Smoke тест проверки заголовка главной страницы"""
     with allure.step("Заголовок главной страницы"):
         assert main_page.check_page_title(TITLE)
 
@@ -32,12 +36,52 @@ def test_check_main_page_title(main_page):
 @allure.story("UI")
 @allure.title("Поиск фильмов по названию")
 @pytest.mark.parametrize("film_title",
-                         ["Аватар", "Такси 4"])
-def test_search_book_by_title(main_page, film_title):
+                         ["Интерстеллар", "Начало", "Матрица"])
+def test_search_film_by_title(main_page, film_title):
+    """Тест поиска фильма по названию на главной странице"""
     with allure.step(f"Поиск фильмов по названию"
                      f" {film_title}"):
         main_page.search_items_by_phrase(film_title)
-    with allure.step("Количество результатов больше 0"):
-        assert main_page.get_search_results_count() > 0
-    with allure.step(f"Название фильма {film_title} содержится в результатах"):
-        assert film_title in main_page.find_book_titles()
+    with allure.step("Проверяет, что количество результатов больше 0"):
+        assert main_page.get_search_results_count() > 0, 'Не удалось получить список фильмов'
+    with allure.step(f"Получение списка найденных фильмов"):
+        assert film_title in main_page.find_films_titles(), "Фильм не найден в результатах поиска"
+        # film_titles = main_page.find_films_titles()
+        # assert len(film_titles) > 0, "Не удалось получить список фильмов"
+
+@allure.feature("Выбор")
+@allure.story("UI")
+@allure.title("Выбор фильма и просмотр")
+@pytest.mark.parametrize("film_title", ["Интерстеллар"])
+def test_choosing_and_watching_movie(main_page, film_title):
+    """Тест выбора и просмотра фильма через поиск на главной странице"""
+    main_page.search_items_by_phrase(film_title)
+    assert main_page.check_page_title(f"Результаты поиска - Кинопоиск"),\
+        "Не удалось перейти на страницу результатов поиска"
+    assert main_page.get_search_results_count() > 0, f"По запросу не найдено ни одного результата"
+    film_titles = main_page.find_films_titles()
+    # assert len(film_titles) > 0, "Не удалось получить список фильмов"
+
+    with allure.step("Возвращает TRUE, если хотя бы один элемент в генераторе равен TRUE"):
+        film_found = any(film_title.lower() == title.lower() for title in film_titles)
+        assert film_found, "Фильм не найден в результатах поиска"
+        main_page._wait_for_elements(By.CSS_SELECTOR, "[data-type='film']").click()
+        main_page._wait_for_elements(By.XPATH,"//button[contains(text(),"
+                                                        " 'Смотреть') or contains(text(), 'Трейлер')]").click()
+
+@allure.feature("Smoke")
+@allure.story("UI")
+@allure.title("Написание рецензии к фильму")
+@pytest.mark.smoke
+def test_writing_movie_review(main_page, film_title):
+    """"Тест написания рецензии на фильм"""
+    with allure.step(f"Поиск фильма по названию {film_title}"):
+        main_page.search_items_by_phrase(film_title)
+    assert main_page.check_page_title(f"Результаты поиска - Кинопоиск"), \
+        "Не удалось перейти на страницу результатов поиска"
+    # Кликаем на первый фильм в результатах поиска
+    main_page._wait_for_elements(By.CSS_SELECTOR, "[data-type='film']").click()
+    # Ждем загрузки страницы фильма
+    main_page._wait_for_elements(By.CSS_SELECTOR, "[data-test-id='film-page']").click()
+    # Прокручиваем до раздела рецензий
+    reviews_button = main_page.driver
